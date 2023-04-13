a8f53110a21a74f29789f5842de1bb3226c1d673
haha861924 <qq861924@gmail.com>
2023-04-13 08:16:01 +0800

    feat: 🎸 show mdx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,15 @@ import Link from 'next/link';
 
 // add the React Header Element
 const Header: React.FC = () => {
-
-    return (
-        // header value
-        <header className="py-2">
-
-        <Link href="/">
-          <h1 className='text-2xl font-bold text-green-500'>吠吠的部落格ㄋㄟˇ</h1>
-        </Link>
-        </header>
-    )
-}
+  return (
+    // header value
+    <header className="py-2">
+      <Link href="/">
+        <h1 className="text-2xl font-bold text-green-500">吠吠的部落格ㄋㄟˇ</h1>
+      </Link>
+    </header>
+  );
+};
 
 // export Header module
 export default Header;
